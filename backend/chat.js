ebf36816b2dd262c4df23d68abe2ca19b88ee81d
MinsunCho-dev <cms081222@gmail.com>
2023-02-11 18:08:28 +0900

    feat: Chat 기본 구조 생성

---
+++ backend/chat.js
@@ -0,0 +1,45 @@
+const chat = require("../socket");
+
+chat.on("connection", async (socket) => {
+  socket.on("enterLobby", (nickname, cb) => {
+    console.log(`${nickname} 로비 입장`);
+    const msg = `${nickname} 님이 입장하셨습니다.`;
+    const msgId = new Date().getTime().toString(36);
+    chat.sockets.emit("receiveLobbyMsg", { notice: msg }, msgId);
+    cb();
+  });
+
+  socket.on("sendLobbyMsg", (payload, cb) => {
+    console.log("로비 채팅");
+    console.log("payload:::", payload);
+    const msgId = new Date().getTime().toString(36);
+    chat.sockets.emit("receiveLobbyMsg", payload, msgId);
+    cb();
+  });
+
+  // 룸 입장 메세지
+  socket.on("enterRoomMsg", (roomNum, nickname, cb) => {
+    console.log(`${nickname} ${roomNum}번 방 입장(enterRoomMsg)`);
+    const msg = `${nickname} 님이 입장하셨습니다.`;
+    const msgId = new Date().getTime().toString(36);
+    chat.sockets.emit("receiveRoomMsg", { notice: msg }, msgId, roomNum);
+
+    cb();
+  });
+
+  socket.on("leaveRoomMsg", (roomNum, nickname) => {
+    console.log(`${nickname} ${roomNum}번 방 퇴장(leaveRoomMsg)`);
+    const msg = `${nickname} 님이 퇴장하셨습니다.`;
+    const msgId = new Date().getTime().toString(36);
+    chat.sockets.emit("receiveRoomMsg", { notice: msg }, msgId, roomNum);
+  });
+
+  socket.on("sendRoomMsg", (payload, roomNum, cb) => {
+    console.log("룸채팅");
+    console.log("payload:::", payload);
+    console.log(`roomNum::: ${roomNum}`);
+    const msgId = new Date().getTime().toString(36);
+    chat.sockets.emit("receiveRoomMsg", payload, msgId, roomNum);
+    cb();
+  });
+});
---
+++ backend/redis.js
@@ -0,0 +1,11 @@
+require("dotenv").config();
+const Redis = require("ioredis");
+
+const redis = new Redis({
+  host: process.env.REDIS_HOST,
+  port: process.env.REDIS_PORT,
+  username: process.env.REDIS_USERNAME,
+  password: process.env.REDIS_PASSWORD,
+});
+
+module.exports = redis;
