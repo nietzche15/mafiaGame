871628d43d54783eceec09427c06ae3e1d0b5268
박현목 <phmok1@gmail.com>
2023-02-15 15:58:30 +0900

    hm as

---
+++ frontend/src/components/Kakao.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import { useEffect } from "react";
+import axios from "axios";
+import qs from "qs";
+import { useNavigate } from 'react-router-dom';
+
+export default function Kakao() {
+
+    const code2 = new URL(window.location.href).searchParams.get("code");
+    console.log('code2 : ' + code2)
+
+    const Auth = () => {
+        const REST_API_KEY = `${process.env.REACT_APP_REST_API_KEY}`;
+        const REDIRECT_URI = `${process.env.REACT_APP_REDIRECT_URI}`;
+        const CLIENT_SECRET = `${process.env.REACT_APP_CLIENT_SECRET}`;
+        // calllback으로 받은 인가코드
+        const code = new URL(window.location.href).searchParams.get("code");
+        const navigate = useNavigate();
+        const getToken = async () => {
+            const payload = qs.stringify({
+                grant_type: "authorization_code",
+                client_id: REST_API_KEY,
+                redirect_uri: REDIRECT_URI,
+                code: code,
+                client_secret: CLIENT_SECRET,
+            });
+            console.log(code)
+            try {
+                // access token 가져오기
+                const res = await axios.post(
+                    "https://kauth.kakao.com/oauth/token",
+                    payload
+                );
+
+                // Kakao Javascript SDK 초기화
+                window.Kakao.init(REST_API_KEY);
+                // access token 설정
+                window.Kakao.Auth.setAccessToken(res.data.access_token);
+                navigate.replace("/profile");
+            } catch (err) {
+                console.log(err);
+            }
+        };
+        useEffect(() => {
+            getToken();
+        }, []);
+    }
+
+
+
+    // const code = new URL(window.location.href).searchParams.get("code")
+
+
+    return (
+        <div>g</div>
+
+    )
+}
