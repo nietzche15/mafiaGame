e013215038386622dac374f0cadced86e7d8ecca
dolahee <155641@naver.com>
2023-02-16 18:17:27 +0900

    채팅방 입장 알림

--- backend/index.js
+++ backend/index.js
@@ -17,17 +17,14 @@ app.get('/', (req, res) => {
 io.on('connection', (socket) => {
   console.log('Server Socket Connected', socket.id);
 
+  socket.emit('joinNotice', `${socket.id} 님이 접속하셨네요. 할일없는듯`);
   // 같은 방 입장한 회원 분류 roomID - socket.id
-  socket.on('join', function (message) {
-    console.log(message);
-    // const rooms = io.sockets.adapter.rooms;
-    // const room = rooms.get(roomID);
-  });
-
-  socket.emit('sendJoinMessage', socket.id);
+  // socket.on('join', function (message) {
+  //   console.log(message);
+  // });
 
   socket.on('disconnect', () => {
-    console.log('Server Socket disconnected');
+    console.log(`${socket.id}님과 연결이 끊어졌어요.`);
     // delete
   });
 });
