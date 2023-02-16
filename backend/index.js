2d271d5dd9adfc75f07f67955ce38e74340fda41
nietzche15 <yeongeun93@gmail.com>
2023-02-16 16:56:03 +0900

    Feat: 채팅 서버 기본 설정 진행 중

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,8 @@ const io = require('socket.io')(http, {
   },
 });
 
-let nickNameList = { admin: 'Server' };
+const roomToUser = {}; // roomID - [user1(socekt.id) , user2(socekt.id), user3(socekt.id), ...]
+const userToRoom = {}; // socket.id - roomID
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/../frontend/src/components/Chatting.jsx');
@@ -17,10 +18,38 @@ app.get('/', (req, res) => {
 io.on('connection', (socket) => {
   console.log('Server Socket Connected', socket.id);
 
-  // 같은 방 입장한 회원 분류 roomID - socket.id
-  socket.on('join', function (roomID) {
-    let rooms = io.sockets.adapter.rooms;
-    let room = rooms.get(roomID);
+  // 방 입장
+  // 같은 방 입장한 회원 구분 roomID - socket.id
+  socket.on('join room', (roomID) => {
+    if (roomToUser[roomID]) {
+      const length = roomToUser[roomID].length;
+      if (length === 8) {
+        socket.emit('room full');
+        return;
+      }
+      roomToUser[roomID].push(socket.id);
+    } else {
+      roomToUser[roomID] = [socket.id];
+    }
+    userToRoom[socket.id] = roomID;
+
+    // ----나중에 영상과 연결 시 사용
+    // let rooms = io.sockets.adapter.rooms;
+    // let room = rooms.get(roomID);
+    // if (room === undefined) {
+    //   socket.join(roomName);
+    //   userToRoom[socket.id] = roomID;
+    //   roomToUser[roomID] = [socket.id];
+    // } else if (room.size <= 8) {
+    //   socket.join(roomName);
+    //   userToRoom[socket.id] = roomID;
+    //   roomToUser[roomID].push(socket.id);
+    // } else {
+    //   socket.emit('room full');
+    // }
+
+    const usersInThisRoom = roomToUser[roomID].filter((id) => id !== socket.id);
+    socket.emit('all users', usersInThisRoom);
   });
 
   socket.on('disconnect', () => {
